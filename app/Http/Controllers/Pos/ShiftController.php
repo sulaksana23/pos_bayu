b6c65d422b7da0978550a7c650248152f7a94903
root <root@v10062.prohosting24.de>
2026-07-04 23:17:42 +0200

    fix: tambah import DB facade di ShiftController agar close shift tidak error

--- app/Http/Controllers/Pos/ShiftController.php
+++ app/Http/Controllers/Pos/ShiftController.php
@@ -7,6 +7,7 @@ use App\Models\PosTransaction;
 use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class ShiftController extends Controller
